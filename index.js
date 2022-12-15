a657108217ca7e04b735a5fbcbe2f43d3ce6087e
Shone Binu <shonebinualias@gmail.com>
2022-12-15 18:31:05 +0530

    first push

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//Shift Cipher program which supports both the alphabet cases, default shift is 3 (Caesar Cipher)
+
+const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z']
+
+
