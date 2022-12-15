bfe6d0a3a61878f2efb19ce8437b8462965caa39
Shone Binu <shonebinualias@gmail.com>
2022-12-15 21:10:16 +0530

    starting

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 //Shift Cipher program which supports both the alphabet cases, default shift is 3 (Caesar Cipher)
 
-const upperAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-  'Y', 'Z']
+const upperAlphabets = [
+  'A', 'B', 'C', 'D', 'E', 'F',
+  'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R',
+  'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z'
+]
 
 
 const lowerAlphabets = [
@@ -14,3 +18,18 @@ const lowerAlphabets = [
   's', 't', 'u', 'v', 'w', 'x',
   'y', 'z'
 ]
+
+
+function encrypt(string, shift = 3) {
+
+  let splittedString = string.split("")
+  splittedString.forEach(element => {
+
+    console.log(element)
+
+  })
+
+}
+
+
+console.log(encrypt("ABC"))
