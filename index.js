433aacca74c0f9c4063fe2fdf63e2fd7117b11cf
Shone Binu <shonebinualias@gmail.com>
2022-12-15 18:34:01 +0530

    lower and upper alphabets

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 
 //Shift Cipher program which supports both the alphabet cases, default shift is 3 (Caesar Cipher)
 
-const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+const upperAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
   'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
   'Y', 'Z']
 
 
+const lowerAlphabets = [
+  'a', 'b', 'c', 'd', 'e', 'f',
+  'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r',
+  's', 't', 'u', 'v', 'w', 'x',
+  'y', 'z'
+]
